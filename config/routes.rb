18df591aee532f0656f38e3318c624031514c5c2
activeliang <liang@activeliangdeiMac.local>
2017-02-10 22:26:49 +0800

    get out

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  resources :products do
-    member do
-      post :add_to_cart
-    end
-  end
+     resources :products do
+       member do
+         post :add_to_cart
+       end
+     end
 end
